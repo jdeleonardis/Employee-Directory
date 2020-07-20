838800c703f6781a06f84368a317b16dbab03023
jdeleonardis <jdeleonardis@hotmail.com>
2020-07-20 09:47:49 -0400

    Functioning data table

--- src/components/DataTable.js
+++ src/components/DataTable.js
@@ -2,13 +2,29 @@ import React from "react";
 
 function DataTable(props) {
     return (
-        <ul className="list-group">
-          {props.filteredEmployees.map(employee => (            
-            <li className="list-group-item" key={employee.email}>
-              <img alt="employee pic" src={employee.picture.thumbnail}/> {employee.name.first + " " + employee.name.last} {employee.phone} {employee.email} {employee.dob.date}
-            </li>
-          ))}
-        </ul>
+
+        <table class="table">
+            <thead>
+                <tr>
+                <th scope="col">Pic</th>
+                <th scope="col">Name</th>
+                <th scope="col">Phone</th>
+                <th scope="col">Email</th>
+                <th scope="col">DOB</th>                
+                </tr>
+            </thead>
+            <tbody>
+                {props.filteredEmployees.map(employee => (
+                    <tr className="tr-stuff" key={employee.email}>
+                        <td><img alt="employee pic" src={employee.picture.thumbnail}/></td>
+                        <td>{employee.name.first + " " + employee.name.last}</td>
+                        <td>{employee.phone}</td>
+                        <td>{employee.email}</td>
+                        <td>{employee.dob.date}</td>                        
+                    </tr>
+                ))}
+            </tbody>
+        </table>
       );
 }
 
